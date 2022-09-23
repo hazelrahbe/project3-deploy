d4bc1f9d5639d80d7b7949a1037d1925504496d2
hazelrahbe <swp.hazel94@gmail.com>
2022-09-23 09:26:51 -0400

    Carousel testin

--- src/pages/Cocktails.js
+++ src/pages/Cocktails.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-
 const Cocktails = () => {
     const [alldrinks, setDrink] = useState([]);
     const [mySearch, setMySearch] = useState('');
